0e9cdc204fe94f12e93d877f0a42bba5cf6c2547
Ziyaad <rhyman.ziyaad@gmail.com>
2022-04-01 17:18:45 +0400

    [+][ADD] Updated character details

--- src/Components/CharacterDetail/CharacterDetail.js
+++ src/Components/CharacterDetail/CharacterDetail.js
@@ -138,8 +138,9 @@ const CharacterDetail = () => {
                                         <div>
                                             <div className="mb-3 xl:w-96">
                                                 <textarea
-                                                    className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none"
+                                                    className="form-control max-h-24 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0focus:text-gray-700 focus:bg-white focus:border-green-600 focus:outline-none"
                                                     rows="3"
+                                                    maxlength="100"
                                                     placeholder="Enter a quote for this character"
                                                 ></textarea>
                                             </div>
