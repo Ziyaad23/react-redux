7912afeb5b5796628e90b2fe4181035e43d3c92e
Ziyaad <rhyman.ziyaad@gmail.com>
2022-04-02 17:04:02 +0400

    [+][ADD] Updated styles

--- src/Components/CharacterCard/CharacterCard.js
+++ src/Components/CharacterCard/CharacterCard.js
@@ -8,7 +8,7 @@ const CharacterCard = (props) => {
             <div className="flex justify-center md:hover:scale-105">
                 <div className="rounded-lg shadow-lg bg-white">
                     <div>
-                        <img className="rounded-t-lg h-96 md:h-80 w-96 object-cover" src={data.img} alt="characters" />
+                        <img className="rounded-t-lg h-30rem md:h-80 w-96 object-cover" src={data.img} alt="characters" />
                     </div>
                     <div className="p-6 h-32">
                         <h5 className="text-gray-900 text-xl font-medium mb-2">{data.name}</h5>
